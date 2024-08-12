1c4a4849433cfd88346138251850690a98825146
geoffreylarnold <geoffrey.arnold@alleghenycounty.us>
2024-08-12 12:12:01 -0400

    Bookmark_06 demo

---
+++ Class-5/apps/bookmark_06.R
@@ -0,0 +1,45 @@
+library(shiny)
+
+# Define UI for accumulator app -------------------------------------
+ui <- function(request) {
+  fluidPage(
+    sidebarPanel(
+      sliderInput("n", "Value to add", min = 0, max = 100, value = 50),
+      actionButton("add", "Add"), br(), br()
+    ),
+    mainPanel(
+      h4("Sum:", textOutput("sum"))
+    )
+  )
+}
+
+# Define server accumulator app with bookmarking --------------------
+server <- function(input, output, session) {
+  
+  observe({
+    reactiveValuesToList(input)
+    session$doBookmark()
+  })
+  # Update the query string
+  onBookmarked(updateQueryString)
+  
+  vals <- reactiveValues(sum = 0)
+  
+  onBookmark(function(state) {
+    state$values$currentSum <- vals$sum
+  })
+  onRestore(function(state) {
+    vals$sum <- state$values$currentSum
+  })
+  setBookmarkExclude("add")
+  
+  observeEvent(input$add, {
+    vals$sum <- vals$sum + input$n
+  })
+  output$sum <- renderText({
+    vals$sum
+  })
+}
+
+# Run the app -------------------------------------------------------
+shinyApp(ui, server, enableBookmarking = "url")
